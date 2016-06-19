028fe03feccd3eac3592091d2818ec825ec6b850
Arek Sredzki <arek@sredzki.com>
2016-06-19 01:51:13 -0400

    Update binutils to sha256

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -5,13 +5,13 @@ class AvrBinutils < Formula
     homepage 'http://www.gnu.org/software/binutils/binutils.html'
     url 'http://ftpmirror.gnu.org/binutils/binutils-2.26.tar.gz'
     mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz'
-    sha1 'bb933ec42eb2002d9270bd4c62a26c3706589ee9'
+    sha256 '9615feddaeedc214d1a1ecd77b6697449c952eab69d79ab2125ea050e944bcc1'
 
     # Support for -C in avr-size. See issue
     # https://github.com/larsimmisch/homebrew-avr/issues/9
     patch :p0 do
         url 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'
-        sha1 'b6d1ff7084b1f0a3fd2dee5383019ffb202e6c9a'
+        sha256 '1776fa53ec8c8cc6031c205be0ca358db32fccef7831d29d17b3435309ef7a1c'
     end
 
     def install
